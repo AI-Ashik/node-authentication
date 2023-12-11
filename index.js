6e85a5fcd683e035c871a5a697c4a283ee17fda6
AI-Ashik <arifulislamashik07@gmail.com>
2023-12-11 14:48:44 +0600

    added schema validation and salting/hashing password

--- index.js
+++ index.js
@@ -1,73 +1,131 @@
-// database encryption authentication - make changes only in schema
-
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 const User = require("./models/user.model");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-const dbURL = process.env.MONGO_URL;
+const saltRounds = 10;
 
-mongoose
-  .connect(dbURL)
-  .then(() => {
-    console.log("mongodb atlas is connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const { PORT } = process.env;
+const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const { DB_URL } = process.env;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("DB is connected"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}views/index.html`);
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register/", async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json(newUser);
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+      const newUser = new User({
+        username,
+        email,
+        password: hash,
+      });
+
+      await newUser.save();
+      res.status(201).send(newUser);
+    });
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({
+      message: error.message,
+    });
   }
 });
 
-app.post("/login", async (req, res) => {
+// app.post("/login/", async (req, res) => {
+//   const { username, email, password } = req.body;
+//   try {
+//     const userU = await User.findOne({ username });
+//     const userE = await User.findOne({ email });
+
+//     if (userU) {
+//       bcrypt.compare(password, userU.password, (err, result) => {
+//         if (result === true) {
+//           res.status(200).json({
+//             message: "Valid User",
+//           });
+//         }
+//       });
+//     } else if (userE) {
+//       bcrypt.compare(password, userE.password, (err, result) => {
+//         if (result === true) {
+//           res.status(200).json({
+//             message: "Valid User",
+//           });
+//         }
+//       });
+//     } else {
+//       res.status(500).json({
+//         message: "Not a valid User",
+//       });
+//     }
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// });
+app.post("/login/", async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user && user.password === password) {
-      res.status(200).json({ status: "valid user" });
+    const userU = await User.findOne({ username });
+    const userE = await User.findOne({ email });
+
+    const user = userU || userE;
+
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result === true) {
+          res.status(200).json({
+            message: "Valid User",
+          });
+        } else {
+          res.status(500).json({
+            message: "Not a valid User",
+          });
+        }
+      });
     } else {
-      res.status(404).json({ status: "Not valid user" });
+      res.status(500).json({
+        message: "Not a valid User",
+      });
     }
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({
+      message: error.message,
+    });
   }
 });
 
-// route not found error
-app.use((req, res, next) => {
+app.get((req, res, next) => {
   res.status(404).json({
-    message: "route not found",
+    message: "Route Not Found",
   });
   next();
 });
-
-// handling server error
-app.use((err, req, res, next) => {
+app.get((error, req, res, next) => {
   res.status(500).json({
-    message: "something broke",
+    message: "Internal Server Error",
   });
   next();
 });
-
+// server listen
 app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
--- models/user.model.js
+++ models/user.model.js
@@ -1,25 +1,40 @@
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+
+// email validation
+const validateEmail = (email) => {
+  const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
 
 const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, "Username is required"],
+    unique: [true, "username is already used"],
+  },
   email: {
     type: String,
-    require: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: [true, "Email address is required"],
+    validate: [validateEmail, "{VALUE} is not a valid email address"],
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
   },
   password: {
     type: String,
-    require: true,
+    trim: true,
+    required: [true, "Password is required"],
+    minLength: [8, "Password must be minimum 8 characters"],
   },
-  createdOn: {
+  createdAt: {
     type: Date,
     default: Date.now,
   },
-});
-
-const encKey = process.env.ENC_KEY;
-userSchema.plugin(encrypt, {
-  secret: encKey,
-  encryptedFields: ["password"],
 });
 
 module.exports = mongoose.model("user", userSchema);
